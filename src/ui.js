5c6e5ca1dd92f8311236e237d503ccf865da93fc
JJ2102 <baumann08jonathan@gmail.com>
2025-08-29 10:16:48 +0200

    add btn

--- src/ui.js
+++ src/ui.js
@@ -1,24 +1,19 @@
 function initUI() {
-  const panel = document.createElement("div");
-  panel.id = "my-lss-panel";
-  panel.style.position = "fixed";
-  panel.style.top = "100px";
-  panel.style.right = "20px";
-  panel.style.backgroundColor = "white";
-  panel.style.border = "1px solid black";
-  panel.style.padding = "10px";
-  panel.style.zIndex = 1000;
+  const btnID = "min_btn";
+  const btnContent = "Select min";
+  const alarmBtn = document.getElementById("alert_btn");
 
-  // Button erstellen
-  const button = document.createElement("button");
-  button.id = "my-button";
-  button.innerText = "Klick mich!";
-  panel.appendChild(button);
+  if (alarmBtn) {
+    if (document.getElementById(btnID)) return;
 
-  document.body.appendChild(panel);
+    const btn = document.createElement("button");
+    btn.id = btnID;
+    btn.textContent(btnContent);
+    btn.classList.add("btn", "btn-success");
+    alarmBtn.parentNode.insertBefore(btn, alarmBtn);
 
-  // Button-Event
-  button.addEventListener("click", () => {
-    startAction();
-  });
+    btn.addEventListener("click", () => {
+      alert("Btn Gedrückt");
+    });
+  }
 }
