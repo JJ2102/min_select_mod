f8feeb0762d14eb092b49c4b9ede3f6d249273ac
JJ2102 <baumann08jonathan@gmail.com>
2025-08-30 14:52:24 +0200

    add grand for GM_xmlhttpRequest

--- scripts/lss-mod.user.js
+++ scripts/lss-mod.user.js
@@ -5,7 +5,7 @@
 // @description  Demo Mod mit Button
 // @author       JJ Baumann
 // @match        https://www.leitstellenspiel.de/*
-// @grant        none
+// @grant        GM_xmlhttpRequest
 // @require      https://raw.githubusercontent.com/JJ2102/min_select_mod/main/src/logic.js
 // @require      https://raw.githubusercontent.com/JJ2102/min_select_mod/main/src/ui.js
 // @require      https://raw.githubusercontent.com/JJ2102/min_select_mod/main/src/main.js
