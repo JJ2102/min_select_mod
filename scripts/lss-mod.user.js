8677f42a14401c7db53d8ade3ac02f97a7061307
JJ2102 <baumann08jonathan@gmail.com>
2025-08-31 21:49:02 +0200

    add some ids

--- scripts/lss-mod.user.js
+++ scripts/lss-mod.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Mein LSS Mod
 // @namespace    http://tampermonkey.net/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Demo Mod mit Button
 // @author       JJ
 // @match        https://www.leitstellenspiel.de/*
